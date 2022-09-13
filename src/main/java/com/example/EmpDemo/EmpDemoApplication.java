d623662d09bfbaccb9eb2d7e123cbaedec30f84d
Prasad Tamarapalli <v-s-b-e.prasad-tamarapalli@capgemini.com>
2022-09-13 09:43:30 +0530

    Commiting EmpDemo code

---
+++ src/main/java/com/example/EmpDemo/EmpDemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.EmpDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpDemoApplication.class, args);
+
+	System.out.println("Hi this is first emp demo");
+
+	}
+}
